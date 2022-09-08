d5b8adb0bd12e2b0772862ced61e508e65cfa356
_Zaizen_ <leonardocanello@protonmail.com>
2022-09-08 14:59:17 +0200

    Scrape senatori

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-CSV_FILENAME: str = "deputati.csv"
+CSV_FILENAME_DEPUTATI: str = "deputati.csv"
+CSV_FILENAME_SENATORI: str = "senatori.csv"
 # Pagina contenente un menù a tendina con nomi, cognomi e id di tutti i deputati
 DEPUTIES_URL = 'https://www.camera.it/leg18/28'
 
@@ -73,13 +74,47 @@ def scrape() -> list[tuple[str, str, str, str]]:
     print(f'\nI deputati sono {deputies_number}, di cui {deputies_with_email} dotati di indirizzo e-mail perché in carica e {deputies_without_email} no perché cessati dal mandato parlamentare.')
     return rows
 
+def scrape_senatori():
+
+    rows: list[tuple[str, str, str, str]] = []
+    
+    char = 'a'
+    for i in range(0,28):        
+        url = "https://www.senato.it/leg/18/BGT/Schede/Attsen/Sen" + char + ".html"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        senatori = soup.find_all("div", {"class": "senatore"})
+        
+        for senatore in senatori:
+            elem = senatore.select_one(":nth-child(2)").p.a
+            cognome, nome = elem.get_text().lower().split(" ", 1)
+            id = senatore.select_one(":nth-child(1)").img["src"][-12:][:-4]
+            link_senatore = "https://www.senato.it/leg/18/BGT/Schede/Attsen/" + id + ".htm"
+            r = requests.get(link_senatore)
+            writeTo = BeautifulSoup(r.text, 'lxml')
+            mail = writeTo.find_all("ul", {"class": "composizione contatti"})
+
+            email = str(mail)[124:].split("'", 1)[0]
+
+            print(f'{id:7} {cognome:20} {nome:20} {email}')
+
+            rows.append((id, cognome, nome, email))
+
+        char = chr(ord(char)+1)
+
+    return rows
 
 def main() -> None:
-    create_csv_file(CSV_FILENAME)
+    create_csv_file(CSV_FILENAME_SENATORI)
+    create_csv_file(CSV_FILENAME_DEPUTATI)
+    
+    rows = scrape_senatori()
+    write_csv(CSV_FILENAME_SENATORI, rows)
 
     rows = scrape()
 
-    write_csv(CSV_FILENAME, rows)
+    write_csv(CSV_FILENAME_DEPUTATI, rows)
 
 
 if __name__ == "__main__":
